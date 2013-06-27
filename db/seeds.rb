af97ff7a6aeeefdf03dce73a0778744f132f30f6
Eric Szeto <eszeto@gmail.com>
2013-06-27 11:18:14 -0400

    created real views, made productscontroller respond to html and json requests differently

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create!(
+	name: "Box",
+	description: "Sturdy Box",
+	price_in_cents: 1000
+)
