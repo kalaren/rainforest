cfdcfb00cb6213965f027bdaf48f003e3fd457ac
Eric Szeto <eszeto@gmail.com>
2013-06-27 11:36:39 -0400

    created shared error rendering page for add and edit prodcut form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price_in_cents
+
+  validates :name, :description, presence: true
+  validates :price_in_cents, numericality: {only_integer: true}
 end
