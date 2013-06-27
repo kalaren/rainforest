cad6f8ec6d0e03f13a69f58181ed22bcfc435147
Eric Szeto <eszeto@gmail.com>
2013-06-27 16:04:48 -0400

    created review model added relationshipns

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price_in_cents
+  has_many :reviews
+  had_many :users, through: :reviews
 
   validates :name, :description, presence: true
   validates :price_in_cents, numericality: {only_integer: true}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
   has_secure_password
+  has_many :reviews
+  has_many :products, through: :reviews
+
   validates_presence_of :password, on: :create
 end
